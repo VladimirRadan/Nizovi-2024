3f92ce9e3f733419f6581c8cbe121acf7ee60949
Vladimir Radan <vladimir.radan@1nce.com>
2024-04-05 19:41:51 +0200

    Added arrays

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,58 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int [] niz = new int[3];
+        niz[0] = 5;
+        niz[1] = 6;
+        niz[2] = 7;
 
-        System.out.println("Ispis");
+//        for (int i = 0; i < niz.length; i++) {
+//            System.out.println(niz[i]);
+//        }
+
+
+//        for (int a : niz){// hasNext
+//            System.out.println(a);
+//        }
+
+
+
+
+        String [] s = {"Marko", "Ivan", "Pera"};// ------>[Ljava.lang.String;@65ab7765
+
+        System.out.println(s);
+
+
+        int mojNiz[] = {3, 5, 7, 9, 10};
+
+
+        int [][] niz2d = new int[][]{
+                {1,2,8},
+                {3,4,10},
+                {5,6,15}
+        };
+
+//        for (int a : mojNiz) {
+//            System.out.println(a);
+//        }
+
+        for(int []n: niz2d){
+            for (int a : n){
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
 
 
     }
+
+
 
 
 }
