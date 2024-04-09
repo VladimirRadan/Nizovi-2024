ebbd224859e7e41885c86b11dd974357f91fa682
Vladimir Radan <vladimir.radan@1nce.com>
2024-04-09 18:16:04 +0200

    Added array min and max

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,28 @@
+import java.util.Arrays;
+
 public class Main {
 
 
 
     public static void main(String[] args) {
 
-        int [] niz = new int[3];
-        niz[0] = 5;
-        niz[1] = 6;
-        niz[2] = 7;
 
-//        for (int i = 0; i < niz.length; i++) {
-//            System.out.println(niz[i]);
-//        }
+        int mojNiz[] = {55, 5, 7, 180, 10, 200, 60, 600, 3};
 
+        int max = mojNiz[0];
 
-//        for (int a : niz){// hasNext
-//            System.out.println(a);
-//        }
-
-
-
-
-        String [] s = {"Marko", "Ivan", "Pera"};// ------>[Ljava.lang.String;@65ab7765
-
-        System.out.println(s);
-
-
-        int mojNiz[] = {3, 5, 7, 9, 10};
-
-
-        int [][] niz2d = new int[][]{
-                {1,2,8},
-                {3,4,10},
-                {5,6,15}
-        };
-
-//        for (int a : mojNiz) {
-//            System.out.println(a);
-//        }
-
-        for(int []n: niz2d){
-            for (int a : n){
-                System.out.print(a + " ");
+        for (int i = 0; i < mojNiz.length; i++) {
+            if (mojNiz[i] > max){
+                max = mojNiz[i];
             }
-            System.out.println();
         }
 
+        System.out.println(max);
 
+        Arrays.sort(mojNiz);
 
-
-
+        System.out.println(mojNiz[0]);
+        System.out.println(mojNiz[mojNiz.length-1]);
 
 
 
